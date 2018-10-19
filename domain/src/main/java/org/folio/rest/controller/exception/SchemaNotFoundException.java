80b0caa56e461e5a5c9f568e97fdc584b8c64389
William Welling <wwelling@library.tamu.edu>
2018-10-19 12:41:34 -0500

    serve up schemas

---
+++ domain/src/main/java/org/folio/rest/controller/exception/SchemaNotFoundException.java
@@ -0,0 +1,11 @@
+package org.folio.rest.controller.exception;
+
+public class SchemaNotFoundException extends RuntimeException {
+
+  private static final long serialVersionUID = -3475304055299328175L;
+
+  public SchemaNotFoundException(String message) {
+    super(message);
+  }
+
+}
