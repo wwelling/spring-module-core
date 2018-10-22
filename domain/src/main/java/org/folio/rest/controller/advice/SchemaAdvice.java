557c7ca22112ffc04c9f48d27fc2e5783a917001
William Welling <wwelling@library.tamu.edu>
2018-10-22 10:35:44 -0500

    handle io exception for object mapping errors

--- domain/src/main/java/org/folio/rest/controller/advice/SchemaAdvice.java
+++ domain/src/main/java/org/folio/rest/controller/advice/SchemaAdvice.java
@@ -1,5 +1,7 @@
 package org.folio.rest.controller.advice;
 
+import java.io.IOException;
+
 import org.folio.rest.controller.exception.SchemaNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,6 +14,12 @@ public class SchemaAdvice {
   @ResponseStatus(HttpStatus.NOT_FOUND)
   @ExceptionHandler(SchemaNotFoundException.class)
   public String handleSchemaNotFoundException(SchemaNotFoundException exception) {
+    return exception.getMessage();
+  }
+
+  @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+  @ExceptionHandler(IOException.class)
+  public String handleIOException(IOException exception) {
     return exception.getMessage();
   }
 
