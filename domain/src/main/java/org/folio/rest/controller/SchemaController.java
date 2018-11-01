3dbd67a8ccd280b61c8af4f2ed0e8cf3fa8f8135
William Welling <wwelling@library.tamu.edu>
2018-11-01 15:32:58 -0500

    improved exception handling

--- domain/src/main/java/org/folio/rest/controller/SchemaController.java
+++ domain/src/main/java/org/folio/rest/controller/SchemaController.java
@@ -3,6 +3,7 @@ package org.folio.rest.controller;
 import java.io.IOException;
 import java.util.Map;
 
+import org.folio.rest.controller.exception.SchemaIOException;
 import org.folio.rest.service.SchemaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,13 +21,21 @@ public class SchemaController {
   private SchemaService schemaService;
 
   @GetMapping
-  public Map<String, JsonNode> getSchemas() throws IOException {
-    return schemaService.getSchemas();
+  public Map<String, JsonNode> getSchemas() throws SchemaIOException {
+    try {
+      return schemaService.getSchemas();
+    } catch (IOException e) {
+      throw new SchemaIOException("Unable to get schemas!", e);
+    }
   }
 
   @GetMapping("/{name}")
-  public JsonNode getSchemaByName(@PathVariable String name) throws IOException {
-    return schemaService.getSchemaByName(name);
+  public JsonNode getSchemaByName(@PathVariable String name) throws SchemaIOException {
+    try {
+      return schemaService.getSchemaByName(name);
+    } catch (IOException e) {
+      throw new SchemaIOException(String.format("Unable to get schema %s!", name), e);
+    }
   }
 
 }
--- domain/src/main/java/org/folio/rest/controller/advice/SchemaAdvice.java
+++ domain/src/main/java/org/folio/rest/controller/advice/SchemaAdvice.java
@@ -1,8 +1,9 @@
 package org.folio.rest.controller.advice;
 
-import java.io.IOException;
-
+import org.folio.rest.controller.exception.SchemaIOException;
 import org.folio.rest.controller.exception.SchemaNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -11,15 +12,19 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class SchemaAdvice {
 
+  private static final Logger logger = LoggerFactory.getLogger(SchemaAdvice.class);
+
   @ResponseStatus(HttpStatus.NOT_FOUND)
   @ExceptionHandler(SchemaNotFoundException.class)
   public String handleSchemaNotFoundException(SchemaNotFoundException exception) {
+    logger.debug(exception.getMessage(), exception);
     return exception.getMessage();
   }
 
   @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-  @ExceptionHandler(IOException.class)
-  public String handleIOException(IOException exception) {
+  @ExceptionHandler(SchemaIOException.class)
+  public String handleSchemaIOException(SchemaIOException exception) {
+    logger.debug(exception.getMessage(), exception);
     return exception.getMessage();
   }
 
