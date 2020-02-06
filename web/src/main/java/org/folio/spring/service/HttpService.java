42261d40045f91fa4558f1682539cfe1fce305b2
William Welling <wwelling@library.tamu.edu>
2020-02-06 07:42:27 -0600

    need to get values on post construct

--- web/src/main/java/org/folio/spring/service/HttpService.java
+++ web/src/main/java/org/folio/spring/service/HttpService.java
@@ -2,8 +2,11 @@ package org.folio.spring.service;
 
 import java.time.Duration;
 
+import javax.annotation.PostConstruct;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpEntity;
@@ -23,11 +26,15 @@ public class HttpService {
   @Value("${http.read-timeout:3600}")
   private int readTimeout;
 
+  @Autowired
+  private RestTemplateBuilder restTemplateBuilder;
+
   private RestTemplate restTemplate;
 
-  public HttpService(RestTemplateBuilder restTemplateBuilder) {
-    logger.info("Rest template connection timeout: {}", connectionTimeout);
-    logger.info("Rest template read timeout: {}", readTimeout);
+  @PostConstruct
+  public void setup() {
+    logger.info("Rest template connection timeout: {} seconds", connectionTimeout);
+    logger.info("Rest template read timeout: {} seconds", readTimeout);
     this.restTemplate = restTemplateBuilder
         .setConnectTimeout(Duration.ofSeconds(connectionTimeout))
         .setReadTimeout(Duration.ofSeconds(readTimeout))
