e41f53be7f9323a4e1695b577c9b67fef575eb97
William Welling <wwelling@library.tamu.edu>
2020-02-06 07:38:51 -0600

    log rest template timeouts

--- web/src/main/java/org/folio/spring/service/HttpService.java
+++ web/src/main/java/org/folio/spring/service/HttpService.java
@@ -2,6 +2,8 @@ package org.folio.spring.service;
 
 import java.time.Duration;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpEntity;
@@ -13,6 +15,8 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class HttpService {
 
+  private static final Logger logger = LoggerFactory.getLogger(HttpService.class);
+
   @Value("${http.connection-timeout:3600}")
   private int connectionTimeout;
 
@@ -22,6 +26,8 @@ public class HttpService {
   private RestTemplate restTemplate;
 
   public HttpService(RestTemplateBuilder restTemplateBuilder) {
+    logger.info("Rest template connection timeout: {}", connectionTimeout);
+    logger.info("Rest template read timeout: {}", readTimeout);
     this.restTemplate = restTemplateBuilder
         .setConnectTimeout(Duration.ofSeconds(connectionTimeout))
         .setReadTimeout(Duration.ofSeconds(readTimeout))
